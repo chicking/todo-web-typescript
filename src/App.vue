177b7c3195af93b091ce36793f9a07327d124fc6
gongzza <gz.us.to@gmail.com>
2017-07-22 14:05:13 +0900

    Add single component file with decorator
    - vue-property-decorator를 이용해서 Decorator를 사용
    - Decorator사용을 위해서 tsconfig 수정

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <div>Hello {{name}}!</div>
+    Name: <input v-model="name" type="text">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  public name: string = 'World'
+}
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import Vue from "vue";
+import App from './App.vue'
 
-let v = new Vue({
-    el: "#app",
-    template: `
-    <div>
-        <div>Hello {{name}}!</div>
-        Name: <input v-model="name" type="text">
-    </div>`,
-    data: {
-        name: "World"
-    }
-});
+const app = new App()
+app.$mount('#app')
