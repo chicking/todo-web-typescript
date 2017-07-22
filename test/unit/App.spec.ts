01e02240b7b76491bba442874723ae09f9b557bb
gongzza <gz.us.to@gmail.com>
2017-07-22 14:51:54 +0900

    Refactor test
    - add @types/mocha
    - test element textContent

--- test/unit/App.spec.ts
+++ test/unit/App.spec.ts
@@ -2,11 +2,22 @@ import App from '@/App.vue'
 import { expect } from 'chai'
 
 describe('App.vue', () => {
-  it('should equal name', () => {
+  it('should equal name', done => {
     const vm = new App({
       el: document.createElement('div')
     })
 
     expect(vm.name).to.equals('World')
+
+    const $firstDiv = vm.$el.querySelector('div:first-child')
+
+    expect($firstDiv).to.not.be.null
+    expect($firstDiv.textContent).to.equals('Hello World!')
+
+    vm.name = 'Vue'
+    vm.$nextTick(() => {
+      expect($firstDiv.textContent).to.equals('Hello Vue!')
+      done()
+    })
   })
 })
