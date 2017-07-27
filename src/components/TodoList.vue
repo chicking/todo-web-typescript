80ef2bb77a34d72a472e21bde0d885340b31bd76
gongzza <gz.us.to@gmail.com>
2017-07-27 09:47:20 +0900

    Revert "Change decorator library"
    This reverts commit e647c64b2a68e096dc98962543a0f0fed1d314fb.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -45,7 +45,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Component from 'vue-class-component'
+import { Component } from 'vue-property-decorator'
 import { Todo } from '@/models'
 
 @Component
